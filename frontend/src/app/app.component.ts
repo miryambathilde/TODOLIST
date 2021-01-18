e974ec96af69f8f9bbfb345e2b931acc378da51c
Miryam Bathilde <mbathilde@gmail.com>
2021-01-18 15:47:20 +0100

    instalacion de angular material

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: 'Hola {{title}} <tareas></tareas><mat-slider min="1" max="100" step="1" value="1"></mat-slider>',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,15 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TareasComponent } from './tareas.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
 
 @NgModule({
   declarations: [
     AppComponent,
+    TareasComponent
   ],
+
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
