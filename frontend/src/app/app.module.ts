4cd29be17e902bc456de0aff317af279337f18d6
Miryam Bathilde <mbathilde@gmail.com>
2021-01-18 15:13:11 +0100

    genero componente tareas

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TareasComponent } from './tareas/tareas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TareasComponent
   ],
   imports: [
     BrowserModule,
