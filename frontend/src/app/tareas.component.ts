eafc64d5a787edc2288eb83da97c49bd9b81ad92
Miryam Bathilde <mbathilde@gmail.com>
2021-01-19 15:46:36 +0100

    montamos server con node expresss

--- frontend/src/app/tareas.component.ts
+++ frontend/src/app/tareas.component.ts
@@ -2,7 +2,7 @@ import {  Component } from "@angular/core"
 
 @Component ({
     selector: 'tareas',
-    template: `<h1>Listado tareas</h1>
+    template: `<h1>Listado de tareas</h1>
         <mat-card *ngFor="let tarea of tareas" style="margin:8px">
         <mat-card-title>{{tarea.usuario}}</mat-card-title>
         <mat-card-content>
