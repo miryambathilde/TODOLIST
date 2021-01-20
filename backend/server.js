edcb01f4092274d59b07e17d58de319943a4284b
Miryam Bathilde <mbathilde@gmail.com>
2021-01-20 10:45:02 +0100

    instalacion nodemon + cambio de ruta en server.js

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,16 @@
 var express = require('express');
 var app = express();
 
-/* aqui manejamos una peticion cuando el usuario haga una peticion (req) a la raiz
-de mi app, y le enviare una respuesta cuando reciba la peticion */
-app.get('/', (req, res)=>{
-    res.send('Hola Miryam');
+//creamos la variable tareas con el objeto que tenemos en tareas.component.ts//
+var tareas = [{trabajo: 'primera tarea', usuario: 'Miryam'},
+            {trabajo: 'segunda tarea', usuario: 'Oliver'}];
+
+/*peticion a tareas y ponemos que devuelva de respuesta un json y le pasamos las tareas*/
+app.get('/tareas', (req, res)=>{
+    res.json(tareas); //
 })
 
 /* para arrancar la aplicacion de express tenemos que utilizar un puerto, podemos
 poner el puerto que queramos siempre que no coincida con otro puerto que estemos utilizando */
 app.listen(1234);
+
