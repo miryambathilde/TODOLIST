7829b0b5fa61708d210b4f3b209b8e067a0dd208
Miryam Bathilde <mbathilde@gmail.com>
2021-01-20 12:14:01 +0100

    JSON POSTMAN request + body Parser JSON - Express

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var cors = require('cors'); //cors
+var bodyParser = require('body-parser') //parseador JSON para express
 var app = express();
 //variable CORS con su configuración SINGLE CORS
 var corsOpt = {
@@ -12,12 +13,21 @@ var tareas = [{trabajo: 'Primera tarea', usuario: 'Miryam Bathilde'},
             {trabajo: 'Segunda tarea', usuario: 'Oliver Crevillen'}];
 
 
+/* METODOS */
+app.use(bodyParser.json()); //ponemos nuestro metodo para el PARSEADOR JSON
 
 /*peticion a tareas y ponemos que devuelva de respuesta un json y le pasamos las tareas*/
 app.get('/tareas', cors(corsOpt),(req, res)=>{
-    res.json(tareas); //
+    res.json(tareas); 
 })
 
+/*POST, va a tener una peticion que va a tener un body donde yo voy a 
+escribir la tercera tarea por ejemeplo. Una vez que yo muestre los datos,
+respondere con un estatus 200 si esta todo OK */
+app.post('/tarea', cors(corsOpt),(req, res)=>{
+    console.log(req.body); 
+    res.sendStatus(200);
+})
 /* para arrancar la aplicacion de express tenemos que utilizar un puerto, podemos
 poner el puerto que queramos siempre que no coincida con otro puerto que estemos utilizando */
 app.listen(1234);
